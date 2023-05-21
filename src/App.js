0002b0e5d4d738cd15d6048d215090832c6854df
Baskar P <baskarp31@gmail.com>
2023-05-21 19:36:21 +0530

    second time

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./Navbar";
+import Service from "./Services";
+import Portfolio from "./Portfolio"
+import About from "./About";
+import Team from "./Team";
+import Clients from "./Clients";
+import Footer from "./Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () =>{
+
+    return(
+
+        <div>
+        <Navbar/>
+        <Service/>
+        <Portfolio/>
+        <About/>
+        <Team/>
+        <Clients/>
+        <Footer/>
+        </div>
+    );
+
 }
-
 export default App;
---
+++ src/README.md
@@ -0,0 +1,1 @@
+## GD Predict Site ##
